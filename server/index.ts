e62c66fd9279532433701e1e4f329c2cfa264658
shara <sharapov.savely@yandex.ru>
2025-05-14 09:23:40 +0300

    add: test socket connection

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,30 @@
-console.log('INIT TYPESCRIP SERVER');
+import express from "express";
+import {createServer} from "http";
+import {Server} from "socket.io";
+
+const PORT = process.env.PORT || 3000;
+const __prod = process.env.NODE_ENV === "production";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: __prod ? "" : "http://localhost:5173",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("Client connected");
+})
+
+httpServer.listen(PORT, () => {
+    if (__prod) {
+        console.log(
+            `(Production Mode) Client and server is running under http://localhost:${PORT}`
+        );
+    } else {
+        console.log(`Server is running under development port ${PORT}`);
+    }
+});
